121ff9006bbf838ea7bfab4fb70038b86705ee01
malunkun <209446860@qq.com>
2019-01-15 21:45:55 +0800

    update my client code

--- client.c
+++ client.c
@@ -15,6 +15,28 @@
 
 int main(int argc,char *argv[])
 {
+	int optret;
+	int ipaddr;
+	int port_set;
+	while((optret = getopt(argc,argv,"i:p:")) != -1)
+	{
+		switch(optret)
+		{
+			case 'i':
+				printf("option = i,the ip will be set=%s\n",optarg);
+				break;
+			case 'p':
+				printf("option = p,the port will be set=%s\n",optarg);
+				break;
+		}
+	}
+	if (socket(AF_INET,SOCK_STREAM,0) < 0)
+	{
+		perror("socket fail:");
+		return -1;
+	}
+	printf("socket successful!\n");
 
+	return 0;
 }
 
