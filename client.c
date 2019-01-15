5732ec823ba2d07c1f8fb37719c0f3f93e683454
malunkun <209446860@qq.com>
2019-01-15 14:27:17 +0800

    add client code

---
+++ client.c
@@ -0,0 +1,20 @@
+/*************************************************************************
+    > File Name: client.c
+    > Author: malunkun
+    > Mail: malunkun<209446860qq.com> 
+    > Created Time: 2019年01月15日 星期二 14时13分53秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<sys/types.h>  
+#include<sys/socket.h>
+#include<signal.h>
+#include<unistd.h>
+
+int main(int argc,char *argv[])
+{
+
+}
+
