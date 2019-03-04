a44dea5509586e5610a4e9eac199875e0983569a
zhanghang <1711318490@qq.com>
2019-03-04 14:53:43 +0000

    update

--- server.c
+++ server.c
@@ -7,6 +7,7 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include "sqlite3.h"
 
 #define PORT 9005
 #define BUFFSIZE 1024
@@ -101,7 +102,6 @@ void *pthread_func(void *args)
 		}
 		printf("%s\n",buf);
 		sqlite_ret = sqlite_func((char *)&buf);
-		printf("数据库\n");
 	} 
 		close(plisten_fd);
 		return NULL;
@@ -115,8 +115,10 @@ int sqlite_func(char *temper)//存入数据库
 	char ID_buf[20];
 	char temp_buf[20];
 	char data_buf[50];
+    char *str[3];
 	int i = 0;
 	char *sql = "";
+    char buf[1024];
 	char *delim="|";
 	char *p=strtok(temper,delim);
 	if(NULL == p)
@@ -127,23 +129,13 @@ int sqlite_func(char *temper)//存入数据库
 	while(p != NULL)
 	{
 		printf("%s\n",p);
+        str[i] = p;
 		p = strtok(NULL,delim);
-		i++;
-		if(i=1)
-		{
-			strncpy(ID_buf,p,sizeof(p));
-		}
-		if(i=2)
-		{
-			strncpy(temp_buf,p,sizeof(p));
-		}
-		if(i=3)
-		{
-			strncpy(data_buf,p,sizeof(p));
-		}
-		if(i>3)
-			i=0;
+        i = i+1;
+        if(i>=3)
+            i=0;
 	}
+    printf("%s %s %s",str[0],str[1],str[2]);
 	ret = sqlite3_open("./server.db",&db);
 	if(ret != SQLITE_OK)
 	{
@@ -157,14 +149,14 @@ int sqlite_func(char *temper)//存入数据库
 		printf("create table fail:%s",errmsg);
 		return -1;
 	}
-	sql = "insert into server values(ID_buf,temp_buf,data_buf);"
-
-	ret = sqlite3_exec(db,sql,NULL,NULL,&errmsg);
+    printf("here\n");
+	sprintf(buf,"insert into server values('%s','%s','%s');",str[0],str[1],str[2]);
+    printf("%s\n",buf);
+	ret = sqlite3_exec(db,(char *)&buf,NULL,NULL,&errmsg);
 	if(ret != SQLITE_OK)
 	{
 		printf("insert values fail:%s",errmsg);
 		return -1;
 	}
-
 	return 0;
 }
