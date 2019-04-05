53ce5d0f64e5087dd0f30d972293ca5ea571d44e
malunkun <209446860@qq.com>
2019-04-05 21:36:32 +0800

    update

--- epoll_/server/do_epoll.c
+++ epoll_/server/do_epoll.c
@@ -12,7 +12,7 @@ int epoll_set(int lieten_fd,struct epoll_event *event,char *msg)
 	}
 	event.events = EPOLLIN|EPOLLET;
 	event.data.fd= listen_fd;
-	ret = epoll_ctl(epfd,EPOLL_CTL_ADD,listen_fd,&event);
+	ret = epoll_ctl(epfd,EPOLL_CTL_ADD,listen_fd,event);
 	if(ret < 0)
 	{
 		sprintf(msg,"epoll_ctl fail:%s\n",strerror(errno));
@@ -24,13 +24,14 @@ int epoll_set(int lieten_fd,struct epoll_event *event,char *msg)
 int handle_events(int epfd,int listenfd,struct sockaddr_in *addr,socklen_t *addrlen,int eventnum,struct epoll_event *event,char *sendbuf)
 {
 	int fd;
+	int ret = -1;
 	for(int i=0;i<eventnum;i++)
 	{
 		fd = event[i].date.fd;
 		if((fd&EPOLLERR)||(fd&EPOLLHUP))
 		{
 			printf("有挂断或出错\n");
-			epoll_ctl(epfd,EPOLL_CTL_DEL,fd,&event);
+			epoll_ctl(epfd,EPOLL_CTL_DEL,fd,event);
 			close(fd);
 		}
 		else if(fd = listenfd)
@@ -39,10 +40,51 @@ int handle_events(int epfd,int listenfd,struct sockaddr_in *addr,socklen_t *addr
 			if(accept < 0)
 			{
 				printf("accept fail!%s\n",strerror(errno));
-				epoll_ctl(epfd,EPOLL_CTL_DEL,fd,&event);
+				epoll_ctl(epfd,EPOLL_CTL_DEL,fd,event);
 				close(fd);
 			}
-			read(fd)
+			ret = read(acceptfd,sendbuf,BUFFSIZE);
+			if(ret < 0)
+			{
+				printf("read fail%s\n",strerror(errno));
+				return -1;
+			}
+			if(ret = 0)
+			{
+				printf("连接断开\n");
+				close(fd);
+				break;
+			}
+			ret = write(acceptfd,sendbuf,BUFFSIZE);
+			if(ret < 0)
+			{
+				printf("write fail\n");
+				return -1;
+			}
+			epoll_ctl(epfd,EPOLL_CTL_ADD,acceptfd,event);
+		}
+		else
+		{
+			printf("已连接fd%d\n",fd);
+			ret = read(fd,sendbuf,BUFFSIZE);
+			if(ret < 0)
+			{
+				printf("read fail%s\n",strerror(errno));
+				return -1;
+			}
+			if(ret = 0)
+			{
+				printf("连接断开\n");
+				close(fd);
+				break;
+			}
+			ret = write(fd,sendbuf,BUFFSIZE);
+			if(ret < 0)
+			{
+				printf("write fail\n");
+				return -1;
+			}
 		}
 	}
+	return 0;
 }
